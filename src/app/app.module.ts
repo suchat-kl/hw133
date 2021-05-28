d90fb6657ed3b183ae594749cd29e5f94fd7051f
suchat-kl <suchat.kl@kkumail.com>
2021-05-28 11:37:07 +0700

    complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { InfoComponent } from './info/info.component';
+import { RegisterComponent } from './register/register.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    InfoComponent,
+    RegisterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
